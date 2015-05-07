1d50c77f35c8c5108d57a4b3444f18d5ac008986
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-05-07 15:32:39 -0400

    Add users and posts databases, models

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config') #app reads config.py file
+db = SQLAlchemy(app) #creates db object that will be our database
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -8,3 +8,12 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#path of database file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Where SQLAlchemy-migrate data files stored
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
