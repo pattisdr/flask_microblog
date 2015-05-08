2b694f1bd9d0057eb5ea6a2b77d3377508cb683e
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-05-08 11:18:41 -0400

    Setup followers and following associations

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from app import db
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,7 +13,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
-
+    followed = db.relationship('User',
+                           secondary=followers,
+                           primaryjoin=(followers.c.follower_id == id),
+                           secondaryjoin=(followers.c.followed_id == id),
+                           backref=db.backref('followers', lazy='dynamic'),
+                           lazy='dynamic')
     @staticmethod
     def make_unique_nickname(nickname):
         if User.query.filter_by(nickname=nickname).first() is None:
@@ -40,8 +49,25 @@ class User(db.Model):
         except NameError:
             return str(self.id)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
 
 
 class Post(db.Model):
